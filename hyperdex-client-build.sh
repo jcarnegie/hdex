da8191a91c19f980f518b10fddfe331269047e06
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-06-16 09:47:24 -0700

    Updated build script

--- hyperdex-client-build.sh
+++ hyperdex-client-build.sh
@@ -4,10 +4,9 @@ set -e
 
 HYPERDEX_VERSION=1.7.1
 
-pushd .
 cd deps
 tar zxf hyperdex-${HYPERDEX_VERSION}.tar.gz
 cd hyperdex-${HYPERDEX_VERSION}/bindings/node.js
 node-gyp configure build
 cp build/Release/hyperdex-client.node ../../..
-popd
+cd ../../..
