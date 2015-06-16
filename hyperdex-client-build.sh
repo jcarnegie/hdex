6127a29467e597ac904b748d4ed26cfc0970e9e6
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-06-16 09:59:43 -0700

    Copy hyperdex node module one directory up

--- hyperdex-client-build.sh
+++ hyperdex-client-build.sh
@@ -8,5 +8,4 @@ cd deps
 tar zxf hyperdex-${HYPERDEX_VERSION}.tar.gz
 cd hyperdex-${HYPERDEX_VERSION}/bindings/node.js
 node-gyp configure build
-cp build/Release/hyperdex-client.node ../../..
-cd ../../..
+cp build/Release/hyperdex-client.node ../../../..
